5a06d95ee0aba27d5ce7a6888ea0a579502f9bb9
Mohkith Balaji R B <mohkithbalaji2006@gmail.com>
2025-07-01 19:20:22 +0530

    Update main.py

--- Level_3/reports/Report_Generation/main.py
+++ Level_3/reports/Report_Generation/main.py
@@ -1,9 +1,12 @@
+from flask import Flask
 from jinja2 import Environment, FileSystemLoader
 import json
 
-
 with open("E://BUG BOUNTY/example.json", "r") as f:  # Enter your own path where your output file is saved
     Recon_data = json.load(f)
+
+
+# Use this code to get an output file named recon_report.html
 
 env= Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('base.html')
@@ -11,3 +14,16 @@ html_report = template.render(Recon_data=Recon_data)
 
 with open("recon_report.html", "w") as f:
     f.write(html_report)   
+
+# Use this code to view the output in a website
+
+app = Flask(__name__,template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('base.html', Recon_data=Recon_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+
